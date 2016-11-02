0990d3d058e7a3323bd1a455d980afb99951aa7c
Alain <alain@8786.solutions>
2016-11-02 19:49:20 +0800

    With autoform

--- client/view/careerAppForm.js
+++ client/view/careerAppForm.js
@@ -10,3 +10,45 @@ Template.basicInfo.onRendered(function() {
         defaultDate: moment().format()
     });
 });
+
+Template.basicInfo.events({
+   'click .selItem'(event){
+       posApplied = event.target.text;
+       console.log(event.target.text);
+    }
+});
+
+Template.basicInfo.helpers({
+   posApplied(text){
+       return text;
+   }
+});
+
+Books = new Mongo.Collection("books");
+Books.attachSchema(new SimpleSchema({
+    title: {
+        type: String,
+        label: "Title",
+        max: 200
+    },
+    author: {
+        type: String,
+        label: "Author"
+    },
+    copies: {
+        type: Number,
+        label: "Number of copies",
+        min: 0
+    },
+    lastCheckedOut: {
+        type: Date,
+        label: "Last date this book was checked out",
+        optional: true
+    },
+    summary: {
+        type: String,
+        label: "Brief summary",
+        optional: true,
+        max: 1000
+    }
+}));
